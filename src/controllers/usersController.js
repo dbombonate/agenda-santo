c0eec5be50eb469a4517f955e43e3e47263aff41
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-07-17 12:42:26 -0300

    Add all routes

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -23,13 +23,18 @@ router.post('/register', async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
+    // Checagem de dados da requisição
     if (!validator.isEmail(email)) return res.status(400).send({ error: 'Invalid email.' });
     if (name.length === 0) return res.status(400).send({ error: 'Invalid name.' });
     if (password.length === 0) return res.status(400).send({ error: 'Invalid password.' });
     if (password.length < 6) return res.status(400).send({ error: 'Password must have 6 or more characters' });
 
-    const user = await User.create(req.body);
+    // Checagem se email já está cadastrado
+    const userExists = await User.findOne({ email }).exec();
+    if (userExists) return res.status(400).send({ error: 'Email already in use.' });
 
+    // Cria o usuário e exibe no retorno.
+    const user = await User.create(req.body);
     return res.send(user);
   } catch (err) {
     console.log(err);
@@ -39,5 +44,10 @@ router.post('/register', async (req, res) => {
 
 // Rota de edição de usuários
 router.get('/:id', async (req, res) => res.send('Rota de edição de usuário.'));
+
+// Rota para deletar usuário
+router.delete('/:id', (req, res) => {
+  res.send('Rota para apagar usuário.');
+});
 
 module.exports = (app) => app.use('/users', router);
