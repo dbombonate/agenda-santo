6f5211bff5e42a781c854c2ad8fa14126523dc7e
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-07-17 00:49:19 -0300

    Controller Users

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,3 +1,18 @@
 const express = require('express');
 
 const User = require('../models/User');
+
+const router = express.Router();
+
+// Rota para listar usuários
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find({});
+
+    if (users.length === 0) return res.status(400).send({ alert: There are no users to exibit});
+
+    return res.status(200).send(users);
+  } catch (err) {
+
+  }
+});
