3867412d0240031f3ce1076f40e850136d10bd2e
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-07-27 22:38:49 -0300

    Add list contact by ID route

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -6,6 +6,9 @@ const router = express.Router();
 // Rota para listar Contatos
 router.get('/', (req, res) => res.send({ message: 'GET Contacts' }));
 
+// Rota para listar Contatos por ID
+router.get('/:id', (req, res) => res.send({ message: `GET Contact ID: ${req.params.id}` }));
+
 // Rota para criar Contatos
 router.post('/register', (req, res) => res.send({ message: 'POST Contacts' }));
 
