21f530e33bd40e084457e54c49709762e9e31be3
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-07-31 14:11:22 -0300

    Add OK connection database message

--- src/database/index.js
+++ src/database/index.js
@@ -5,11 +5,10 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.CONNECTION_DB, { useUnifiedTopology: true, useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://teste:daniel@localhost:27017/admin', { useNewUrlParser: true, useUnifiedTopology: true });
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  // we're connected!
+  console.log('MongoDB Connection OK.');
 });
 
 module.exports = mongoose;
