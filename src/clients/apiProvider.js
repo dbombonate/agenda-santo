18877a6b3346bd0297f7298c0bc1d923030b4679
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-08-02 22:22:41 -0300

    Dot ENV variable

--- src/clients/apiProvider.js
+++ src/clients/apiProvider.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
 
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ7XCJ1c2VyTmFtZVwiOlwiZGFuaWVsLmJvbWJvbmF0ZVwiLFwic2VjcmV0XCI6XCJkSFV2QkNyd3FOZU9uak1VdUFqUFd4em5xYnJMbnhJR0ZIZmpRaHhLb2xcIn0ifQ.hyXPSL7BtGMFwdLCxSG6033G_-Tc3ETczo-Fv9RtYbMiDPS9X0yrt_GHa1L718TL9WyPhHyCZQgYq9V3HQcSdg';
+import('dotenv/config');
+
+const token = process.env.TOKEN_BS;
 
 // Cria rota para fazer requisições na Bluesoft no endereço
 
